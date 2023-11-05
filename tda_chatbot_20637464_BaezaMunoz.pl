f56c973242cf010f7c228c2742cc430d376ae358
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-11-04 23:04:26 -0300

    Comentarios y creacion de los selectores
    Se comento y se creacion los selectores del chatbot

--- tda_chatbot_20637464_BaezaMunoz.pl
+++ tda_chatbot_20637464_BaezaMunoz.pl
@@ -1,2 +1,39 @@
+% Constructor.
+% Predicado Nombre: chatbot
+% Dominio: ChatbotID x Name x WelcomeMessage x StartFlowID x Flows x Chatbot
+% Descripción: Crea un chatbot.
+% Meta Primaria: chatbot/6
+% Meta Secundaria: verificarIdRepetidos/3.
+
 chatbot(ChatbotID, Name, WelcomeMessage, StartFlowID, Flows, [ChatbotID, Name, WelcomeMessage, StartFlowID, NewFlows]):-
     verificarIdRepetidos(Flows, [], NewFlows).
+
+
+% Selector.
+% Predicado Nombre: getChatbotId
+% Dominio: Chatbot.
+% Descripción: Obtiene el identificador unico de un chatbot.
+% Meta Primaria: getChatbotId/2
+
+getChatbotId([ChatbotId,_,_,_,_],ChatbotId).
+
+% Selector.
+% Predicado Nombre: getChatbotName
+% Dominio: chatbot.
+% Descripción: Obtiene el nombre de un chatbot.
+% Meta Primaria: getChatbotName/2
+
+getChatbotName([_,Name,_,_,_], Name).
+
+% Selector.
+% Predicado Nombre: getChatbotWelcomeMessege
+% Dominio: Chatbot.
+% Descripción: Obtiene el mensaje de bienvenida de un chatbot.
+% Meta Primaria: getChatbotWelcomeMessege/2
+getChatbotWelcomeMessege([_,_,WelcomeMessage,_,_], WelcomeMessage).
+
+% Selector.
+% Predicado Nombre: getChatbotStartFlowID
+% Dominio: Chatbot.
+% Descripción: Obtiene el identificador del flujo inicial de un chatbot.
+% Meta Primaria: getChatbotStartFlowID/2
