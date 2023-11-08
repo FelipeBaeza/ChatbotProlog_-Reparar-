5c1ca28a2ad9880caff0ef733f0170b69c48e2f6
FelipeBaeza <felipe.baeza.m@usach.cl>
2023-11-07 21:58:42 -0300

    modificaciones tda option
    se agregaron comentarios

--- tda_option_20637464_BaezaMunoz.pl
+++ tda_option_20637464_BaezaMunoz.pl
@@ -1,5 +1,29 @@
-% Constructor de option
+
+
+% Constructor.
+% Nombre Predicado: Option
+% Dominio: Code x Message x ChatbotCodeLink x InitialFlowCodeLink x Keyword, option
+% Descripcion: Crea una Option
+% Meta Primaria: option/6
 option(Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword, [Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword]).
 
-% Selector para obtener el c骴igo de una opci髇
+% Selector.
+% Predicado nombre: getCode.
+% Dominio: Option.
+% Descripci贸n: Obtiene el Code de una opci贸n.
+% Meta Primaria: getCode/2.
 getCode([Code|_], Code).
+
+% Selector.
+% Predicado nombre: getChatbotCodeLink.
+% Dominio: Option.
+% Descripci贸n: Obtiene el ChatbotCodeLink de una opci贸n.
+% Meta Primaria: getCode/2.
+getChatbotCodeLink([_,_,ChatbotCodeLink,_,_], ChatbotCodeLink).
+
+% Selector.
+% Predicado nombre: getInitialFlowCodeLink.
+% Dominio: Option.
+% Descripci贸n: Obtiene el InitialFlowCodeLink de una opci贸n.
+% Meta Primaria: getInitialFlowCodeLink/2.
+getInitialFlowCodeLink([_,_,_,InitialFlowCodeLink,_],InitialFlowCodeLink).
